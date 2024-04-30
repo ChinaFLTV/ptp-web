cdc22a5188123e2081487afc2e501351737b1b02
ChinaFLTV <236010069@qq.com>
2024-04-30 22:13:49 +0800

    2024-4-30  22:13-integrate spring cloud gateway into spring cloud-1

---
+++ ptp-web-service/ptp-web-service-gateway/src/main/java/ptp/fltv/web/service/gateway/GatewayApplication.java
@@ -0,0 +1,25 @@
+package ptp.fltv.web.service.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lenovo/LiGuanda
+ * @version 1.0.0
+ * @date 2024/4/30 PM 6:53:37
+ * @description 网关服务的主启动类
+ * @filename GatewayApplication.java
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatewayApplication.class, args);
+
+    }
+
+
+}
